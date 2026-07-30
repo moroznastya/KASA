db3b52d6af87f3aa17c5dc2be3a9848f7212cce4
Анастасія <nastya.moroz2210@gmail.com>
2026-07-30 07:15:39 +0300

    fix: remove tauri-plugin-updater to fix startup crash

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -24,7 +24,6 @@ pub fn run() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         // Реєстрація команд
         .invoke_handler(tauri::generate_handler![
